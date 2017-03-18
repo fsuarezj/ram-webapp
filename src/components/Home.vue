dfb1335a98c50d36b54d8fd03e2febbeb2f8c94f
Fernando Suárez <fsuarezj@gmail.com>
2017-03-19 00:19:01 +0100

    Added vue-i18n support

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <h1>{{ $t("common.ramName") }}</h1>
+    <h2>The web app</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,30 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueI18n from 'vue-i18n'
 
 Vue.config.productionTip = false
+
+var locales = {
+  en: {
+    common: {
+      ramName: 'Rapid Assessment for Markets'
+    }
+  },
+  es: {
+    common: {
+      ramName: 'Evaluación Rápida de Mercados'
+    }
+  }
+}
+
+Vue.use(VueI18n)
+
+Vue.config.lang = 'es'
+
+Object.keys(locales).forEach(function (lang) {
+  Vue.locale(lang, locales[lang])
+})
 
 /* eslint-disable no-new */
 new Vue({
